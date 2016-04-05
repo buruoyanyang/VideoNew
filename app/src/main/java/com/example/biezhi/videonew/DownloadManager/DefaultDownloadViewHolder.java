5d041c6dba6443c5935176024cf0c2d39224becc
buruoyanyang <xu18716352164@gmail.com>
2016-04-05 21:37:16 +0800

    android studio test

---
+++ app/src/main/java/com/example/biezhi/videonew/DownloadManager/DefaultDownloadViewHolder.java
@@ -0,0 +1,50 @@
+package com.example.biezhi.videonew.DownloadManager;
+
+
+import android.view.View;
+import android.widget.Toast;
+
+import org.xutils.common.Callback;
+import org.xutils.x;
+
+import java.io.File;
+
+/**
+ * Created by xuhuanfeng on 16/4/5.
+ */
+
+public class DefaultDownloadViewHolder extends DownloadViewHolder {
+
+    public DefaultDownloadViewHolder(View view, DownloadInfo downloadInfo) {
+        super(view, downloadInfo);
+    }
+
+    @Override
+    public void onWaiting() {
+
+    }
+
+    @Override
+    public void onStarted() {
+
+    }
+
+    @Override
+    public void onLoading(long total, long current) {
+
+    }
+
+    @Override
+    public void onSuccess(File result) {
+        Toast.makeText(x.app(), "下载完成", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onError(Throwable ex, boolean isOnCallback) {
+        Toast.makeText(x.app(), "下载失败", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onCancelled(Callback.CancelledException cex) {
+    }
+}
